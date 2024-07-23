b689af5a7d9671815a02437075b848d440503aeb
sahil <sahil.saxena@student.manchester.ac.uk>
2024-07-23 17:01:41 +0100

    Layout complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import './style.css';
+import './images/butter-chicken-ac2ff98.jpg';
+import './images/biriyani.jpg';
+import './images/rasmali.jpg';
+
 console.log('Hello');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
+    mode:'development',
     entry: './src/index.js',
     plugins : [
         new HtmlWebpackPlugin({
@@ -11,5 +12,29 @@ module.exports = {
     output: {
         filename : 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'images/[hash][ext][query]',
     },
+    module: {
+
+        rules: [
+    
+          {
+    
+            test: /\.css$/i,
+    
+            use: ['style-loader', 'css-loader'],
+    
+          },
+          {
+
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+    
+            type: 'asset/resource',
+    
+          },
+
+    
+        ],
+    
+      },
 }
